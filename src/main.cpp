a491f59fccfd2736756f1ee2b9029605ede7d326
lucas <lucasjoao.lj@gmail.com>
2019-03-16 22:57:08 -0300

    Set size, position and title to window. Also beautify code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <gtk/gtk.h>
 
 int main (int argc, char *argv[]) {
+
   GtkWidget *window;
 
-  gtk_init (&argc, &argv);
+  gtk_init(&argc, &argv);
 
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), nullptr);
+  gtk_widget_show(window);
+  gtk_widget_set_size_request(window, 800, 600);
+  gtk_window_set_title(GTK_WINDOW(window), "INE5420 - João / Lucas");
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
 
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-
-  gtk_widget_show (window);
-
-  gtk_main ();
+  gtk_main();
 
   return 0;
 }
