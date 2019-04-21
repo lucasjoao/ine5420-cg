321fb5b9cfdd918a3fb494eeb19ab964b8a5aa49
jvitorc <joao.vitor.brasill@gmail.com>
2019-04-20 23:00:58 -0300

    implementacao clipping de ponto

--- src/clipping.hpp
+++ src/clipping.hpp
@@ -46,7 +46,16 @@ void Clipping::clipping(Objeto& obj, int alg) {
         }
 }
 
-void Clipping::ponto(Objeto& obj) {std::cout << "clipping::ponto" << std::endl;}
+void Clipping::ponto(Objeto& obj) {
+    auto c = obj.coordenada_scn(0);
+    if (-1 <= c.valor(Coordenada::x) && c.valor(Coordenada::x) <= 1) {
+        if (-1 <= c.valor(Coordenada::y) && c.valor(Coordenada::y) <= 1) {
+            obj._visivel = true;
+            return;
+        }
+    }
+    obj._visivel = false;
+}
 
 void Clipping::reta_alg0(Objeto& obj) {std::cout << "clipping::reta_alg0" << std::endl;}
 
